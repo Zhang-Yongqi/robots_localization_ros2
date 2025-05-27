3875c42f0593b97dd38f76675f716facd4140d9e
Your Name <KW@openloong.net>
2025-05-27 02:48:44 +0000

    fix warning

--- src/lidar_processor.h
+++ src/lidar_processor.h
@@ -51,14 +51,14 @@ struct orgtype {
 namespace rslidar_ros {
 struct Point {
     PCL_ADD_POINT4D;
-    std::uint8_t intensity;
+    PCL_ADD_INTENSITY;
     uint16_t ring = 0;
     double timestamp = 0;
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 } EIGEN_ALIGN16;
 }  // namespace rslidar_ros
 POINT_CLOUD_REGISTER_POINT_STRUCT(rslidar_ros::Point,
-                                  (float, x, x)(float, y, y)(float, z, z)(uint8_t, intensity, intensity)(
+                                  (float, x, x)(float, y, y)(float, z, z)(float, intensity, intensity)(
                                       uint16_t, ring, ring)(double, timestamp, timestamp))
 
 namespace unilidar_ros {
